39eb14797b9584c39883b453c415c02966163823
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2021-11-09 07:30:48 +0100

    feature: add legacy class longbow.

---
+++ BattleArena/Items/OldVersion/LongBow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BattleArena.Items.OldVersion
+{
+    public class LongBow
+    {
+        private Random randomNumberGenerator;
+        private const int strength = 3;
+
+        public LongBow(Random randomNumberGenerator)
+        {
+            this.randomNumberGenerator = randomNumberGenerator;
+        }
+
+        public int looseAndHit()
+        {
+            int probabilityOfHit = this.haveDayLight() ? 5 : 2;
+            return randomNumberGenerator.Next(10) < probabilityOfHit ? strength : 0;
+        }
+
+        private bool haveDayLight()
+        {
+            int hour = DateTime.Now.Hour;
+            return hour > 6 && hour < 20;
+        }
+    }
+}
